682ca5f5e7eb5f21a2c8478b0b2f94ae25f223e1
David Johnston <dajohnst@thoughtworks.com>
2020-08-18 15:21:39 -0400

    fixed import

--- bikes/bike_sharing_data_ui.py
+++ bikes/bike_sharing_data_ui.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import collections
+import os
 from collections import Counter
 from bikes import data_wrangeling_and_filtering as dwaf
 
